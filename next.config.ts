537baf4f9460b09282c192232bde6fed8fbc5ccc
Shanmoo <surishanmugam@gmail.com>
2025-04-05 07:39:56 -0400

    please

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  assetPrefix: isProd ? '/' : '',
+  assetPrefix: isProd ? '/shanmoo.github.io/' : '',
 };
 
 export default nextConfig;
