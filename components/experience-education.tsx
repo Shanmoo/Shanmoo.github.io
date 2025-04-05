f4f993e075e9e1bd85ea7183c62d0e0e889ccdf4
Shanmoo <surishanmugam@gmail.com>
2025-04-05 18:22:54 -0400

    Update experience-education.tsx

--- components/experience-education.tsx
+++ components/experience-education.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 import Image from "next/image"
-import ScrollAnimationWrapper from "./ScrollAnimationWrapper" // adjust path if needed
+import ScrollAnimationWrapper from "./scroll-animation-wrapper"
 
 type Experience = {
   company: string
