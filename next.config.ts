7f2252f4a13f7b86b039c499ee567ecf343bd987
Shanmoo <surishanmugam@gmail.com>
2025-04-05 04:32:43 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  trailingSlash: true, // Optional: Adds a trailing slash to all routes (e.g., /about/ instead of /about)
+  output: 'export',
+  trailingSlash: true,
 };
 
 export default nextConfig;
